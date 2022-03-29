ce6d7a76a46254309370d7d6b96b3f9053acebf0
wuxiongbo <wuxiongbo@mycs.cn>
2022-03-29 09:30:00 +0800

    调整

--- src/main/java/com/example/annotation/AopUtils.java
+++ src/main/java/com/example/annotation/AopUtils.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -33,37 +32,20 @@ public class AopUtils {
     public static void getControllerMethodDescription(JoinPoint joinPoint)
             throws ClassNotFoundException {
 
-        //获得执行方法所属的类实例
+        // 获得执行方法所属的类实例
         Object target = joinPoint.getTarget();
-        //获得执行方法的类名（全限定名）
+        // Object to class
         Class<?> targetClass = target.getClass();
-        // class to className
-        String className = targetClass.getName();
-        // className to class
-        targetClass = Class.forName(className);
-        // 获取目标类的所有公共方法（不包括私有的）
-        Method[] methods = targetClass.getMethods();
-        System.out.println("切点所在的类名：" + className);
 
+        printTargetClass(targetClass);
 
-        //返回当前连接点签名
+        // 0)获取 切点方法
         Signature signature = joinPoint.getSignature();
-
-        //获得执行方法的方法名方式一（短名）：
-        // 直接 通过 Signature 签名，获取 methodName
-        System.out.println("Signature 获取的方法名："+
-                signature.getName()
-        );
-
-        //获得执行方法的方法名方式二（短名）： **推荐**
-        // 先 通过 Signature 签名 ，获取 Method ；再 通过 Method，获取 methodName
         if (!(signature instanceof MethodSignature)) {
             throw new IllegalArgumentException("该注解只能用于方法");
         }
         MethodSignature methodSignature = (MethodSignature) signature;
         Method targetMethod = methodSignature.getMethod();
-
-
         // 获取 切点方法的
         // 1)方法名
         String targetMethodName = targetMethod.getName();
@@ -77,6 +59,7 @@ public class AopUtils {
         // 5) 参数值 列表。 通过切点获取
         Object[] arguments = joinPoint.getArgs();
 
+
         assert parameterNames != null;
         
         //打印参数
@@ -85,36 +68,15 @@ public class AopUtils {
         // 获取注解
         try {
             // 获取注解：
-            // 先通过 切点方法的 入参类型 以及 方法名，匹配方法签名。
-            // 然后，通过反射技术，直接用目标类对象获取切点方法。
             Method targetMethod1 = targetClass.getMethod(targetMethodName, parameterTypes);
             MyAnnotation annotation = targetMethod1.getAnnotation(MyAnnotation.class);
             //  问题： 通过签名获取的 targetMethod 与 通过反射获取的 targetMethod1 有什么区别呢？
-            //  答：targetMethod 是接口的方法
-            //     targetMethod1 是具体的实现类的方法。
+            //  答： targetMethod 是接口的方法
+            //      targetMethod1 是具体的实现类的方法。
             //     具体原因不清楚。
 
-
-            //还可以获取切点方法中，参数上的注解。
-            // https://www.cnblogs.com/kelelipeng/p/12009680.html
-            //获取方法的 参数上的注解。
-            Annotation[][] parameterAnnotations = targetMethod1.getParameterAnnotations();
-
-            // 遍历参数索引
-            for (Annotation[] parameterAnnotation: parameterAnnotations) {
-
-                // 参数索引位置。
-                int paramIndex= ArrayUtils.indexOf(parameterAnnotations, parameterAnnotation);
-
-                // 遍历当前参数上的全部注解。
-                for (Annotation annotationOnParameter : parameterAnnotation) {
-                    if (annotationOnParameter instanceof RequestParam){
-                        String value = ((RequestParam) annotationOnParameter).value();
-                        System.out.println("参数"+parameterNames[paramIndex]+"上, @RequestParam 注解的值："+value);
-                    }
-                }
-            }
-
+            //读取 切点方法 参数上的注解。
+            printParameterAnnotations(targetMethod1,parameterNames);
 
             if(StringUtils.isNotBlank(annotation.primaryKey())){
                 Resolver resolver = new Resolver(Arrays.asList(parameterNames), arguments);
@@ -129,6 +91,16 @@ public class AopUtils {
         }
     }
 
+    private static void printTargetClass(Class<?> targetClass) throws ClassNotFoundException {
+        // class to className
+        String className = targetClass.getName();
+        // className to class
+        targetClass = Class.forName(className);
+        // 获取目标类的所有公共方法（不包括私有的）
+        Method[] methods = targetClass.getMethods();
+        System.out.println("切点所在的类名：" + className);
+    }
+
     private static void printAnnotation(MyAnnotation annotation,String targetMethodName){
         // 打印后可以看出，获取的annotation 实际上是 Clc 的代理类。
         String annotationName = annotation.getClass().getName();
@@ -140,6 +112,28 @@ public class AopUtils {
         System.out.println("方法"+targetMethodName+"上，注解"+annotationName+"的属性 value 的值为：" + value);
         System.out.println("方法"+targetMethodName+"上，注解"+annotationName+"的属性 name 的值为：" + name);
         System.out.println("方法"+targetMethodName+"上，注解"+annotationName+"的属性 primaryKey 的值为：" + primaryKey);
+    }
+
+
+    private static void printParameterAnnotations(Method targetMethod,String[] parameterNames){
+        // https://www.cnblogs.com/kelelipeng/p/12009680.html
+        //获取方法的 参数上的注解。
+        Annotation[][] parameterAnnotations = targetMethod.getParameterAnnotations();
+
+        // 遍历参数索引
+        for (Annotation[] parameterAnnotation: parameterAnnotations) {
+
+            // 参数索引位置。
+            int paramIndex= ArrayUtils.indexOf(parameterAnnotations, parameterAnnotation);
+
+            // 遍历当前参数上的全部注解。
+            for (Annotation annotationOnParameter : parameterAnnotation) {
+                if (annotationOnParameter instanceof RequestParam){
+                    String value = ((RequestParam) annotationOnParameter).value();
+                    System.out.println("参数"+parameterNames[paramIndex]+"上, @RequestParam 注解的值："+value);
+                }
+            }
+        }
     }
 
     private static void printParameter(String[] parameterNames,Object[] arguments){
